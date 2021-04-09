5cb377fd60f16e8ab37ac4ff8258692d5356e05f
Keni Zhu <zhuk@vcu.edu>
2021-04-09 19:19:37 -0400

    try

--- spiral/homework.py
+++ spiral/homework.py
@@ -1,4 +1,14 @@
 def spiralize(number):
-    return_value = 1
 
+    if number == 1:
+        return_value = 1
+    else:
+        return_value = (
+            4 * (number * number) - 6 * (number - 1) + spiralize(number - 2)
+        )
     return return_value
+
+
+if __name__ == "__main__":
+    print(spiralize(5))
+    print(spiralize(501))
